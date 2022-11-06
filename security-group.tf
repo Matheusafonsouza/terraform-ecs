b605ffc818c69a3d18aff980a795842ca91b9aad
Matheus Afonso <matheusafonsouza@gmail.com>
2022-11-06 18:36:55 -0300

    feat: security group for private communication

--- security-group.tf
+++ security-group.tf
@@ -20,3 +20,26 @@ resource "aws_security_group_rule" "tcp_alb" {
     cidr_blocks = ["0.0.0.0/0"]
     security_group_id = aws_security_group.alb.id
 }
+
+resource "aws_security_group" "private" {
+  name = "private-ecs"
+  vpc_id = module.vpc.vpc_id
+}
+
+resource "aws_security_group_rule" "ecs_in" {
+    type = "ingress"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    source_security_group_id = aws_security_group.alb.id
+    security_group_id = aws_security_group.private.id
+}
+
+resource "aws_security_group_rule" "ecs_out" {
+    type = "egress"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    security_group_id = aws_security_group.private.id
+}
