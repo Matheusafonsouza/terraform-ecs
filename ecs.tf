c5d33f3e7edae7e47f90f68f0a5d50fb6ec12555
Matheus Afonso <matheusafonsouza@gmail.com>
2022-11-25 00:33:04 -0300

    feat: add task definition

--- ecs.tf
+++ ecs.tf
@@ -1,5 +1,29 @@
 module "ecs" {
-    source = "terraform-aws-modules/ecs/aws"
+  source = "terraform-aws-modules/ecs/aws"
+  cluster_name = "ecs-${var.environment}"
+}
 
-    cluster_name = "ecs-${var.environment}"
+resource "aws_ecs_task_definition" "task" {
+  family = "task"
+  requires_compatibilities = [ "FARGATE" ]
+  network_mode = "awsvpc"
+  cpu = 256
+  memory = 512
+  execution_role_arn = aws_iam_role.ecs_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name = "${var.repository_name}-${var.environment}"
+      image = "${aws_ecr_repository.repository.arn}:latest"
+      cpu = 256
+      memory = 512
+      essential = true
+      portMapings = [
+        {
+          containerPort = 8000
+          hostPort = 8000
+        }
+      ]
+    }
+  ])
 }
